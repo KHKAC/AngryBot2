50f054fce404b0ddd5a02b607e37128bad095575
KHKAC <rudgnsrb888@naver.com>
2025-11-14 14:01:12 +0900

    251114 Book End

--- Assets/Scripts/RPCBullet.cs
+++ Assets/Scripts/RPCBullet.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 public class RPCBullet : MonoBehaviour
 {
     public GameObject effect;
+    public int actorNumber;
     void Start()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(
-            Vector3.forward * 1000.0f
-        );
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 1000.0f);
         Destroy(this.gameObject, 3.0f);
     }
 
     void OnCollisionEnter(Collision collision)
     {
         var contact = collision.GetContact(0);
-        var obj = Instantiate(effect,
-                            contact.point,
-                            Quaternion.LookRotation(-contact.normal));
+        var obj = Instantiate(effect, contact.point, Quaternion.LookRotation(-contact.normal));
         Destroy(obj, 2.0f);
         Destroy(this.gameObject);
     }
